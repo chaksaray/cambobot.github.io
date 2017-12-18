531d74d614b96ceca36d0529aeadda5c5005e11d
chaksaray <chaksaray@gmail.com>
2017-12-18 18:38:00 +0700

    custom fb token

--- index.php
+++ index.php
@@ -1,13 +1,8 @@
-<?php 
-	
-	echo "Hello word!!";
+<?php
 
-	if(isset($_REQUEST['hub_challenge'])){
-		$challenge = $_REQUEST['hub_challenge'];
-		$token = $_REQUEST['hub_verify_token'];
-	}
+$challenge = $_REQUEST['hub_challenge'];
+$verify_token = $_REQUEST['hub_verify_token'];
 
-	if($token == "myCustomToken123"){
-		echo $challenge;
-	}
-?>
+if ($verify_token === 'my_token_code') {
+echo $challenge;
+}
